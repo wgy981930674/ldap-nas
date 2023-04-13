8d2dc35621259462c9205d34300c29ca6a1f83c6
wgy981930674 <981930674@qq.com>
2023-04-14 02:55:12 +0800

    second

---
+++ src/Demo.php
@@ -0,0 +1,13 @@
+<?php
+namespace 981930674/ldap-nas;
+/**
+ * Class Demo
+ * @package Xuchen\Biubiubiu
+ */
+class Demo
+{
+    public static function execute()
+    {
+        echo 'It works!';
+    }
+}
